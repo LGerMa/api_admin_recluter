453dc97427c31c467283c41d9ccbf857e40aecb9
Luis M <lgmm.93@gmail.com>
2020-09-06 00:35:37 -0600

    TDD
    Fix TDD optional relation

--- spec/models/job_vacancy_spec.rb
+++ spec/models/job_vacancy_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe JobVacancy, type: :model do
-  it { should belong_to :candidate }
+  it { should belong_to(:candidate).optional }
   it { should belong_to :company_job }
-  it { should belong_to :sale_employee }
-  it { should belong_to :interview_employee }
-  it { should belong_to :payment_employee }
+  it { should belong_to(:sale_employee).optional }
+  it { should belong_to(:interview_employee).optional }
+  it { should belong_to(:payment_employee).optional }
 end
